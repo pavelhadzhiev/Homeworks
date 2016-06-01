3f85560b3bedc7601235b811c5f650b2a9e11499
pavelhadzhiev <P.Hadzhiev96@gmail.com>
2016-06-01 17:53:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Mentor.h"
 #include "QA.h"
 #include "Manager.h"
+#include "Company.h"
 
 using namespace std;
 
@@ -14,21 +15,31 @@ int main()
     Engineer b(a);
     cout << b.get_name() << endl;
     cout << b.getSalary() << endl;
+
     Mentor A;
     A.set_name("Alen");
     A.set_yearsOfService(30);
     A.set_numberOfInterns(25);
     cout << A.get_name() << endl;
     cout << A.getSalary() << endl;
+
     QA B;
     B.set_name("Harry");
     B.set_yearsOfService(20);
     B.set_numberOfTests(120);
     cout << B.get_name() << endl;
     cout << B.getSalary() << endl;
+
     Manager C("Harold", "Boss", 40, 0, 0, 4);
     cout << C.get_name() << endl;
     cout << C.get_title() << endl;
     cout << C.getSalary() << endl;
+
+    Company X;
+    X.add(&a);
+    X.add(&A);
+    X.add(&B);
+    X.add(&C);
+    cout << X.totalSalary() << endl;
     return 0;
 }
